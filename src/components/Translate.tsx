484a8b75c28251a27439d74f53bc99b92ea6af3d
Mohammad Amaren <mohammad.amarena@gmail.com>
2022-11-22 17:19:52 +0100

    add project

--- src/components/Translate.tsx
+++ src/components/Translate.tsx
@@ -1,53 +1,27 @@
 import { useEffect, useState } from 'react';
 import axios from "axios"
 import languagesData from "../../public/languages.json"
-// const axios = require('axios');
 
 export const Translate = () => {
 
   const [languages, setLanguages] = useState(languagesData);
-  const [from, setFrom] = useState('');
+  const [from, setFrom] = useState('en');
   const [to, setTo] = useState('');
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
 
 
-  
-  // useEffect(() => {
-  //   (async () => {
-      
-  //     /* const response = (await axios.get(`https://api.mymemory.translated.net/get?q=${input}!&langpair=${from}|${to}`)).data.matches
-      
-  //     // const _input = response[0].segment
-  //     .then((response) => {
-  //       const _output = response[0].translation
-  //       setOutput(_output)
-  //     })
-      
-  //     // console.log(_output)
-  //     // setInput(_input) */
-      
-    
-  //   // Make a request for a user with a given ID
-  //   axios.get(`https://api.mymemory.translated.net/get?q=hello!&langpair=en|de`)
-  //     .then(function (response) {
-  //       // handle success
-  //       console.log(response.data.matches)
-  //       setOutput(response.data.matches[0].tranlation)
-  //     })
-  //     .catch(function (error) {
-  //       // handle error
-  //       console.log(error);
-  //     })
-  //     .then(function () {
-  //       // setOutput(response)
-  //     });
-  //   })()
-  // }, [])
   const translateBtn = () => {
+    
+    (async () => {
+  
+      const response = (await axios.get(`https://api.mymemory.translated.net/get?q=${input}!&langpair=${from}|${to}`)).data.matches
+      
+      const _output = response[0].translation
+      setOutput(_output)
+    })()
 
-
-    const params = new URLSearchParams();
+    /* const params = new URLSearchParams();
     params.append('q', input);
     params.append('source', from);
     params.append('target', to);
@@ -59,7 +33,7 @@ export const Translate = () => {
       },
     }).then(res => {
       setOutput(res.data.translatedText)
-    })
+    }) */
 
   };
 
